2cda0cddf0130e1bc3dee7fd9296ef3b49e4a602
Thiago <ts.facebook.ts@gmail.com>
2023-01-06 21:28:54 -0300

    Done the mobile responsive to all the pages

--- web/src/App.js
+++ web/src/App.js
@@ -1,89 +1,117 @@
-import React from 'react'
-import {useEffect, useState} from 'react'
-import Slider from './components/Slider';
-import './styles/app.css';
-import image from './assets/title.png'
-import axios from 'axios';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
-import Teams from './pages/Teams';
-import Matches from './pages/Matches';
-import Home from './pages/Home';
-import ErrorPage from './pages/ErrorPage';
-import Standings from './pages/Standings';
+import React from "react";
+import { useEffect, useState } from "react";
+import Slider from "./components/Slider";
+import "./styles/app.css";
+import image from "./assets/title.png";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Teams from "./pages/Teams";
+import Matches from "./pages/Matches";
+import Home from "./pages/Home";
+import ErrorPage from "./pages/ErrorPage";
+import Standings from "./pages/Standings";
 
 function App() {
-
-  let data = [];
+  let isOpen = false;
+  const showMenu = () => {
+    isOpen = !isOpen;
+    console.log(isOpen);
+  };
 
   return (
     <Router>
       <div className="container">
-        <nav className='header-nav'>
-          <div className='header-nav-left'>
-            <Link to="/" className='nav-logo'>
-              <img src={image}/>
+        <nav className={isOpen ? "open-nav" : "header-nav"}>
+          <div className="header-nav-left">
+            <Link to="/" className="nav-logo">
+              <img src={image} />
             </Link>
           </div>
-          <div className='header-nav-right'>
+          <div className="header-nav-right">
+            <button className="btnMenu" onClick={() => showMenu()}>
+              <i class="fa-solid fa-bars"></i>
+            </button>
             <ul>
               <li>
-              <Link to="/teams">
-                Equipos
-              </Link>
+                <Link to="/teams">Equipos</Link>
               </li>
               <li>
-              <Link to="/matches">
-                Partidos
-              </Link>
+                <Link to="/matches">Partidos</Link>
               </li>
               <li>
-              <Link to="/standings">
-                Tablas
-              </Link>
+                <Link to="/standings">Tablas</Link>
               </li>
             </ul>
           </div>
         </nav>
-        <div className='container' style={{
-          margin: '3rem 0'
-        }}>
+        <div
+          className="container"
+          style={{
+            margin: "3rem 0",
+          }}
+        >
           <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/matches' element={<Matches/>}/>
-            <Route path='/teams' element={<Teams/>}/>
-            <Route path='/standings' element={<Standings/>}/>
-            <Route path='*' element={<ErrorPage/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/matches" element={<Matches />} />
+            <Route path="/teams" element={<Teams />} />
+            <Route path="/standings" element={<Standings />} />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
-        <footer style={{
-          fontFamily:'qatar2022',
-          borderTop: '1px solid #AAA'
-          }}>
-          <div className='footer-contact'>
+        <footer
+          style={{
+            fontFamily: "qatar2022",
+            borderTop: "1px solid #AAA",
+          }}
+        >
+          <div className="footer-contact">
             <h3>Contact</h3>
             <h5>Do you like what you see? Let's work together!</h5>
             <ul>
-              <li><a href="mailto:?cc=thiagososaarganaraz@gmail.com?subject=Let's%20work%20together!?&body=Hi!%20I'm%20interested%20in%20working%20with%20you!" target="_blank"><i className="fa-solid fa-inbox"></i></a></li>
-              <li><a href="https://www.linkedin.com/in/thiago-sosa-arga%C3%B1araz-b26034214/"><i className="fa-brands fa-linkedin"></i></a></li>
-              <li><a href="https://github.com/thiaguetas" target="_blank"><i className="fa-brands fa-github"></i></a></li>
+              <li>
+                <a
+                  href="mailto:?cc=thiagososaarganaraz@gmail.com?subject=Let's%20work%20together!?&body=Hi!%20I'm%20interested%20in%20working%20with%20you!"
+                  target="_blank"
+                >
+                  <i className="fa-solid fa-inbox"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/thiago-sosa-arga%C3%B1araz-b26034214/">
+                  <i className="fa-brands fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/thiaguetas" target="_blank">
+                  <i className="fa-brands fa-github"></i>
+                </a>
+              </li>
             </ul>
-            <p style={{
-              fontSize: '12px'
-            }}>powered by <a href='https://github.com/raminmr/free-api-worldcup2022' target='_blank' style={{
-              color: '#9B072E',
-              textShadow: '1px 1px 2px black'
-            }}>WorldCup API ©</a></p>
-            <p style={{
-              fontSize: '12px',
-              textAlign: 'right',
-              marginRight: '1rem',
-              textShadow: '5px 5px 5px black'
-            }}>
+            <p
+              style={{
+                fontSize: "12px",
+              }}
+            >
+              powered by{" "}
+              <a
+                href="https://github.com/raminmr/free-api-worldcup2022"
+                target="_blank"
+                style={{
+                  color: "#9B072E",
+                  textShadow: "1px 1px 2px black",
+                }}
+              >
+                WorldCup API ©
+              </a>
+            </p>
+            <p
+              style={{
+                fontSize: "12px",
+                textAlign: "right",
+                marginRight: "1rem",
+                textShadow: "5px 5px 5px black",
+              }}
+            >
               Thiago Sosa Argañaraz
             </p>
           </div>
--- web/src/pages/Standings.js
+++ web/src/pages/Standings.js
@@ -1,55 +1,55 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import Loading from '../snippets/Loading';
-import '../styles/standings.css'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Loading from "../snippets/Loading";
+import "../styles/standings.css";
 
 function Standings() {
-
   const [standings, setStandings] = useState(null);
   const [selected, setSelected] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     //Pido los datos al renderizar por primera vez
-    async function getData(){
-      try{
-        let res = await axios.get("/standings")
-        console.log(res.data)
-        setStandings(res.data)
+    async function getData() {
+      try {
+        let res = await axios.get("/standings");
+        console.log(res.data);
+        setStandings(res.data);
         setLoading(false);
-      }
-      catch(error){
+      } catch (error) {
         console.log(error.message);
         setLoading(false);
       }
     }
     getData();
-  },[])
+  }, []);
 
   //Renderizo cada vez que selected cambie (que haya un filtro)
-  useEffect(()=>{
-    
-  },[selected])
-
+  useEffect(() => {}, [selected]);
 
   //Algoritmo para ordenar las tablas por puntos
-  let sorted = []
-  standings && standings.map((stand)=> sorted.push((stand.teams).sort((a,b) => {
-    if(a.pts > b.pts) return -1
-    //Si los puntos son iguales, entonces ordenar por diferencia de gol
-    if((a.pts == b.pts) && (a.gd > b.gd)) return -1
-  })))
-
+  let sorted = [];
+  standings &&
+    standings.map((stand) =>
+      sorted.push(
+        stand.teams.sort((a, b) => {
+          if (a.pts > b.pts) return -1;
+          //Si los puntos son iguales, entonces ordenar por diferencia de gol
+          if (a.pts === b.pts && a.gd > b.gd) return -1;
+        })
+      )
+    );
 
   //Funcion para listar las tablas
-  const listStandings = () =>{
+  const listStandings = () => {
     //Si existe un filtro, entonces listar tablas con ese filtro
-    if(selected){
-      return(
-      selected.map((e)=>{
+    if (selected) {
+      return selected.map((e) => {
         return (
-          <div className='standingCard' key={`div${e.group}`}>
-            <h3 style={{textAlign: 'center', margin: '5px'}}>{`Grupo ${e.group}`}</h3>
+          <div className="standingCard" key={`div${e.group}`}>
+            <h3
+              style={{ textAlign: "center", margin: "5px" }}
+            >{`Grupo ${e.group}`}</h3>
             <table key={e.group}>
               <thead>
                 <tr>
@@ -63,114 +63,118 @@ function Standings() {
                   <th>Pts</th>
                 </tr>
               </thead>
-                {
-                  e.teams.map((team)=>{
-                    return(
-                      <tbody>
-                        <tr key={team.id}>
-                          <td><img src={team.flag} style={{width: '40px'}}/> {team.name_en}</td>
-                          <td>{team.gf}</td>
-                          <td>{team.ga}</td>
-                          <td>{team.gd}</td>
-                          <td>{team.w}</td>
-                          <td>{team.l}</td>
-                          <td>{team.mp}</td>
-                          <td>{team.pts}</td>
-                        </tr>
-                      </tbody>
-                    )
-                  })
-                }
+              {e.teams.map((team) => {
+                return (
+                  <tbody>
+                    <tr key={team.id}>
+                      <td>
+                        <img src={team.flag} /> {team.name_en}
+                      </td>
+                      <td>{team.gf}</td>
+                      <td>{team.ga}</td>
+                      <td>{team.gd}</td>
+                      <td>{team.w}</td>
+                      <td>{team.l}</td>
+                      <td>{team.mp}</td>
+                      <td>{team.pts}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
             </table>
           </div>
-        )
-      })
-      )
-    }
-    else{
+        );
+      });
+    } else {
       //De lo contrario, listar todas las tablas :)
-      return(
-        standings.map((e)=>{
-          return (
-            <div className='standingCard' key={`div${e.group}`}>
-              <h3 style={{textAlign: 'center', margin: '5px'}}>{`Grupo ${e.group}`}</h3>
-              <table key={e.group}>
-                <thead>
-                  <tr>
-                    <th>Equipo</th>
-                    <th>GF</th>
-                    <th>GC</th>
-                    <th>DG</th>
-                    <th>PG</th>
-                    <th>PP</th>
-                    <th>PJ</th>
-                    <th>Pts</th>
-                  </tr>
-                </thead>
-                  {
-                    e.teams.map((team)=>{
-                      return(
-                        <tbody key={team.team_id}>
-                          <tr>
-                            <td><img src={team.flag} style={{width: '40px'}}/> {team.name_en}</td>
-                            <td>{team.gf}</td>
-                            <td>{team.ga}</td>
-                            <td>{team.gd}</td>
-                            <td>{team.w}</td>
-                            <td>{team.l}</td>
-                            <td>{team.mp}</td>
-                            <td>{team.pts}</td>
-                          </tr>
-                        </tbody>
-                      )
-                    })
-                  }
-              </table>
-            </div>
-          )
-        })
-      )
+      return standings.map((e) => {
+        return (
+          <div className="standingCard" key={`div${e.group}`}>
+            <h3
+              style={{ textAlign: "center", margin: "5px" }}
+            >{`Grupo ${e.group}`}</h3>
+            <table key={e.group}>
+              <thead>
+                <tr>
+                  <th>Equipo</th>
+                  <th>GF</th>
+                  <th>GC</th>
+                  <th>DG</th>
+                  <th>PG</th>
+                  <th>PP</th>
+                  <th>PJ</th>
+                  <th>Pts</th>
+                </tr>
+              </thead>
+              {e.teams.map((team) => {
+                return (
+                  <tbody key={team.team_id}>
+                    <tr>
+                      <td>
+                        <img src={team.flag} /> {team.name_en}
+                      </td>
+                      <td>{team.gf}</td>
+                      <td>{team.ga}</td>
+                      <td>{team.gd}</td>
+                      <td>{team.w}</td>
+                      <td>{team.l}</td>
+                      <td>{team.mp}</td>
+                      <td>{team.pts}</td>
+                    </tr>
+                  </tbody>
+                );
+              })}
+            </table>
+          </div>
+        );
+      });
     }
-  }
+  };
 
-  const handleSelection = (e) =>{
-    standings && setSelected(standings.filter((stand)=> stand.group === e.target.value))
-  }
+  const handleSelection = (e) => {
+    standings &&
+      setSelected(standings.filter((stand) => stand.group === e.target.value));
+  };
 
   return (
-    <div className='container' style={{fontFamily : 'Qatar2022'}}>
-      <div className='standingsContainer' style={{
-        margin: '0 auto'
-      }}>
-      <nav style={{
-        display: 'flex',
-        justifyContent: 'center',
-        margin: '1rem auto'
-      }}>
+    <div className="container" style={{ fontFamily: "Qatar2022" }}>
+      <nav className="standings-nav">
         <h4>Filtrar por grupo </h4>
-        <select onChange={handleSelection} style={{margin:'auto 1rem', fontFamily:'Qatar2022', fontSize:'18px'}}>
-          {!loading ? standings.map((e)=>{
-            return(
-              <option key={e.group}>{e.group}</option>
-            )
-          })
-          :
-          <option>Cargando...</option>
-          }
+        <select
+          onChange={handleSelection}
+          style={{
+            margin: "auto 1rem",
+            fontFamily: "Qatar2022",
+            fontSize: "18px",
+          }}
+        >
+          {!loading ? (
+            standings.map((e) => {
+              return <option key={e.group}>{e.group}</option>;
+            })
+          ) : (
+            <option>Cargando...</option>
+          )}
         </select>
-        <button style={{
-          fontSize: '20px',
-          margin: '0 1rem'
-        }} onClick={()=>setSelected(null)}>Mostrar todos</button>
+        <button
+          style={{
+            fontSize: "20px",
+            margin: "0 1rem",
+            backgroundColor: "#03090F",
+            color: "white",
+            border: "0.5px solid #999",
+            cursor: "pointer",
+          }}
+          onClick={() => setSelected(null)}
+        >
+          Mostrar todos
+        </button>
       </nav>
-        {!loading ? 
-          listStandings()
-          :
-          <Loading/>
-        }
+      <div className="standingsContainer">
+        {!loading ? listStandings() : <Loading />}
       </div>
     </div>
-  )
+  );
 }
 
-export default Standings
+export default Standings;
