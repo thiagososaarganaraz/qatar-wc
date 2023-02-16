763bf8b9ced004db035ffa115867e8983da61e33
Thiago <ts.facebook.ts@gmail.com>
2023-02-16 12:18:12 -0300

    fixed bug when server not working

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,79 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import image from '../assets/title.png'
-import axios from 'axios';
-import Loading from '../snippets/Loading';
-import '../styles/matches.css'
+import React, { useEffect, useState } from "react";
+import image from "../assets/title.png";
+import axios from "axios";
+import Loading from "../snippets/Loading";
+import "../styles/matches.css";
 
 function Home() {
-
   const [match, setMatch] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-  useEffect(()=>{
-    async function getData(){
-      try{
-        let res = await axios.get("/matches")
-        setMatch(res.data[63])
-        setLoading(false)
-      }catch(error){
-        console.log(error)
-        setLoading(false)
+  useEffect(() => {
+    async function getData() {
+      try {
+        let res = await axios.get("/matches");
+        setMatch(res.data[63]);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+        setError(true);
       }
     }
 
-    getData()
-  },[])
+    getData();
+  }, []);
 
-  const listMatch = () =>{
-    return(
-    <div className='matchCard' key={match.id}>
-      <h4 className='group'>Last Match</h4>
-      <div className='teams'>
-        <div className='teamContent'>
-          <h4>{match.home_team_en}</h4>
-          <div className='matchLocalFlag'><img src={match.home_flag}/></div>
-          <ul className='localScorers'>
-            {
-            match.home_scorers[0] == 'null' ? <li style={{listStyle:'none'}}></li> : (match.home_scorers[0].split(',')).map((scorer)=><li key={scorer+match._id}>{scorer}</li>)
-            }
-          </ul>
-
-        </div>
-        <div className='matchCenterArea'>
-          <h3 className='matchElapsedTime'>{match.time_elapsed}</h3>
-          <div className='matchScore'>
-            <h1 className='localScore'>{match.home_score}</h1><h1>-</h1><h1 className='awayScore'>{match.away_score}</h1>
+  const listMatch = () => {
+    return (
+      <div className="matchCard" key={match?.id}>
+        <h4 className="group">Last Match</h4>
+        <div className="teams">
+          <div className="teamContent">
+            <h4>{match?.home_team_en}</h4>
+            <div className="matchLocalFlag">
+              <img src={match?.home_flag} />
+            </div>
+            <ul className="localScorers">
+              {match?.home_scorers[0] == "null" ? (
+                <li style={{ listStyle: "none" }}></li>
+              ) : (
+                match?.home_scorers[0]
+                  .split(",")
+                  .map((scorer) => <li key={scorer + match?._id}>{scorer}</li>)
+              )}
+            </ul>
+          </div>
+          <div className="matchCenterArea">
+            <h3 className="matchElapsedTime">{match?.time_elapsed}</h3>
+            <div className="matchScore">
+              <h1 className="localScore">{match?.home_score}</h1>
+              <h1>-</h1>
+              <h1 className="awayScore">{match?.away_score}</h1>
+            </div>
+          </div>
+          <div className="teamContent">
+            <h4>{match?.away_team_en}</h4>
+            <div className="matchAwayFlag">
+              <img src={match?.away_flag} />
+            </div>
+            <ul className="awayScorers">
+              {match?.away_scorers[0] == "null" ? (
+                <li style={{ listStyle: "none" }}></li>
+              ) : (
+                match?.away_scorers[0]
+                  .split(",")
+                  .map((scorer) => <li key={scorer + match?.id}>{scorer}</li>)
+              )}
+            </ul>
           </div>
         </div>
-        <div className='teamContent'>
-          <h4>{match.away_team_en}</h4>
-          <div className='matchAwayFlag'><img src={match.away_flag}/></div>
-          <ul className='awayScorers'>
-            {
-            match.away_scorers[0] == 'null' ? <li style={{listStyle:'none'}}></li> : (match.away_scorers[0].split(',')).map((scorer)=><li key={scorer+match.id}>{scorer}</li>)
-            }
-          </ul>
-        </div>
       </div>
-    </div>
-    )
-  }
-
+    );
+  };
 
   return (
     <div className="container">
-    <div className='principal'>
-      <section className='principal-one'>
-        <div className='title'>
-          <img src={image} alt='title' className='title-img'/>
-        </div>
-        <div className='matchContainer'>
-          {!loading ? listMatch() : <Loading/>}
-        </div>
-      </section>
+      <div className="principal">
+        <section className="principal-one">
+          <div className="title">
+            <img src={image} alt="title" className="title-img" />
+          </div>
+          <div className="matchContainer">
+            {!loading ? listMatch() : <Loading />}
+            {error ? <h1>No se pudo conectar con el servidor</h1> : <></>}
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
